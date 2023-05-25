972e75b0772998fe3a3bcba37d54657d0f74a5ed
marcusprodigy <127450777+marcusprodigy@users.noreply.github.com>
2023-05-24 23:43:55 -0300

    ATT

--- src/Components/styles/Inicial/styles.js
+++ src/Components/styles/Inicial/styles.js
@@ -3,21 +3,95 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
 
     container: {
-      
-   
-      flex: 1,
-      padding: '5%',
-  
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-  
+
+      flex:1,
       backgroundColor: '#fff',
       width: '100%',
       height: '100%',
-  
+
     },
-  
-  
-  });
+
+    containerh:{
+        width:'100%',
+        height:'5%',
+        backgroundColor:'#559DED',
+
+
+    },
+    prompt:{
+
+        width:'100%',
+        height:'90%',
+        backgroundColor: '#fff',
+
+        padding:'7%',
+
+    },
+
+    containerf:{
+        width:'100%',
+        height:'5%',
+        backgroundColor:'#559DED',
+        
+    },
+
+    containertext:{
+        flexDirection:'row',
+
+    },
+    textnome:{
+
+        marginLeft:4,
+        color:'#559DED',
+        fontWeight:'bold',
+    },
+    containerbotao:{
+        width:'100%',
+        flexDirection:'row',
+        marginTop:'10%',
+        justifyContent:'space-around',
+        textAlign:'center',
+
+
+
+    },
+
+    botaotela:{
+        width:170,
+        height:170,
+        
+        borderRadius:30,
+        backgroundColor:'#559DED',
+        justifyContent:'center',
+
+        
+
+    },
+
+    Textbotao:{
+        fontSize:20,
+        fontWeight:'bold',
+        color:'#FFF',
+
+    },
+
+    containerquadros:{
+    marginTop:20,
+
+    },
+
+    botaoretangulo:{
+        width:350,
+        height:80,
+        
+        borderRadius:30,
+        backgroundColor:'#559DED',
+        justifyContent:'center',
+
+
+
+    },
+
+});
 
   export default styles;
--- src/Views/Inicial.js
+++ src/Views/Inicial.js
@@ -1,14 +1,35 @@
 import { Text, View } from 'react-native';
 import React from 'react';
 import styles from '../Components/styles/Inicial/styles';
+import { TouchableOpacity } from 'react-native';
 
 function Inicial({ navigation }) {
 
 
     return(
 
-      <View>
-
+      <View style={styles.container}>
+        <View style={styles.containerh}></View>
+        <View style={styles.prompt}>
+          
+          <View style={styles.containertext}><Text>Olá,</Text><Text style={styles.textnome}>Marcus Vinicius Lins Lemos Filho</Text></View>
+          <View style={styles.containerquadros}>
+            <View style={styles.containerbotao}>
+              <TouchableOpacity style={styles.botaotela}><Text style={styles.Textbotao}> Cadastrar</Text><Text style={styles.Textbotao}>Cliente</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.botaotela}><Text style={styles.Textbotao}>Marcar</Text><Text style={styles.Textbotao}>Consulta</Text></TouchableOpacity>
+            </View>
+            
+        
+            <View style={styles.containerbotao}>
+              <TouchableOpacity style={styles.botaotela}><Text style={styles.Textbotao}>Consultas</Text><Text style={styles.Textbotao}>Marcadas</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.botaotela}><Text style={styles.Textbotao}>Criar</Text><Text style={styles.Textbotao}>Comanda</Text></TouchableOpacity>
+            </View>
+            <View style={styles.containerbotao}>
+              <TouchableOpacity style={styles.botaoretangulo}><Text style={styles.Textbotao}>HISTÓRICO DE CONSULTA</Text></TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={styles.containerf}></View>
       </View>
 
       
